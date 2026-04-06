c718fdefdd2ef4d6eee1bdf510b5aad5ea19f20b
Rodrigo Gutierrez Cruz <134517998+ItzRodri@users.noreply.github.com>
2026-04-06 16:05:23 -0400

    fix errores

--- index.js
+++ index.js
@@ -148,6 +148,9 @@ app.use(cors({
     }
 }));
 
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true }));
+
 // --- FIN DEL CAMBIO ---
 
 const storage = multer.memoryStorage();
